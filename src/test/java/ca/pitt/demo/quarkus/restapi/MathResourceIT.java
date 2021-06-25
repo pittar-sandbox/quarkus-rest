a5cfb7fff9023bb57a2920668258f0b98c797cb3
opentlc-mgr <apitt@redhat.com>
2021-06-25 20:02:06 +0000

    Delete native test.

--- src/test/java/ca/pitt/demo/quarkus/restapi/MathResourceIT.java
+++
@@ -1,9 +0,0 @@
-package ca.pitt.demo.quarkus.restapi;
-
-import io.quarkus.test.junit.NativeImageTest;
-
-@NativeImageTest
-public class NativeMathResourceIT extends MathResourceTest {
-
-    // Execute the same tests but in native mode.
-}
