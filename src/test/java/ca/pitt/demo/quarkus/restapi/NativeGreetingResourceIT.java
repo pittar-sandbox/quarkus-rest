d12e46d4a54d2103b2d605eff968cf10ccf57129
opentlc-mgr <apitt@redhat.com>
2021-06-25 19:39:59 +0000

    Fixed native test.

--- src/test/java/ca/pitt/demo/quarkus/restapi/NativeGreetingResourceIT.java
+++ src/test/java/ca/pitt/demo/quarkus/restapi/NativeGreetingResourceIT.java
@@ -3,7 +3,7 @@ package ca.pitt.demo.quarkus.restapi;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeMathResourceIT extends MathResourceTest {
 
     // Execute the same tests but in native mode.
 }
